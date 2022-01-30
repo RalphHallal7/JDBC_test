cec6b827ac02a1326501dc22e6972f4e63c1d30a
RalphHallal7 <rcwhallal2000>
2022-01-31 00:52:44 +0200

    init commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args){
+
+        try {
+
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc_test", "root", "root");
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("select * from students");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("FirstName"));
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
